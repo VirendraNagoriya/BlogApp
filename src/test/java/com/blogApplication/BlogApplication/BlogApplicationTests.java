3cc47a830482102f5bbe943567d9d5771275f37c
Virendra Nagoriya <VirendraNagoriya11@gmail.com>
2025-12-07 19:25:37 +0530

    first Commit to master

---
+++ src/test/java/com/blogApplication/BlogApplication/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogApplication.BlogApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
