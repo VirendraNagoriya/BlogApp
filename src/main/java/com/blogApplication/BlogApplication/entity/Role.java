05932768aa870879f0103bd670c7f12ed383e566
Virendra Nagoriya <VirendraNagoriya11@gmail.com>
2025-12-11 12:27:57 +0530

    post update

--- src/main/java/com/blogApplication/BlogApplication/entity/Role.java
+++ src/main/java/com/blogApplication/BlogApplication/entity/Role.java
@@ -1,8 +1,26 @@
 package com.blogApplication.BlogApplication.entity;
 
 
+import jakarta.persistence.*;
+import lombok.*;
+
 @Getter
 @Setter
-@No
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+
+
+@Table(name = "roles")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+    @c
+private  String name;
+
 }
