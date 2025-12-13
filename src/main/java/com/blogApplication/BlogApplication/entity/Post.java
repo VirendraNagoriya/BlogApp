a2188344278fd913e45d48b53eb30a137b8f0169
Virendra Nagoriya <VirendraNagoriya11@gmail.com>
2025-12-13 13:15:55 +0530

    13-12-noon

--- src/main/java/com/blogApplication/BlogApplication/entity/Post.java
+++ src/main/java/com/blogApplication/BlogApplication/entity/Post.java
@@ -4,7 +4,10 @@ package com.blogApplication.BlogApplication.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.extern.apachecommons.CommonsLog;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.Instant;
 import java.util.List;
 
 @Getter
@@ -28,14 +31,23 @@ private String title;
     @Column(nullable = false , columnDefinition = "TEXT")
 private String content ;
 
+
 private String category;
 
 @ElementCollection
     @CollectionTable(name = "post_tags" , joinColumns = @JoinColumn(name = "post_id"))
     @Column(name = "tags")
+ private List<String> tags;
 
-    private List<String> tags;
 
+
+@CreationTimestamp
+    private  Instant createdAt;
+
+@UpdateTimestamp
+    private Instant updatedAt;
+
+45
 
 
 
