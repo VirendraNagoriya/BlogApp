5fe9f1f5843782780e3aca027f4a7071e64c5e08
Virendra Nagoriya <VirendraNagoriya11@gmail.com>
2025-12-14 20:56:33 +0530

    completed repo- added

--- src/main/java/com/blogApplication/BlogApplication/repository/PostRepository.java
+++ src/main/java/com/blogApplication/BlogApplication/repository/PostRepository.java
@@ -1,4 +1,19 @@
 package com.blogApplication.BlogApplication.repository;
 
-public interface PostRepository {
+import com.blogApplication.BlogApplication.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post ,Long> {
+
+    //serach by tittle and content containing search item
+    Page<Post> findByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(
+
+            String titleTerm,
+            String contentTerm,
+            Pageable pageable
+
+    ) ;
+
 }
--- src/main/java/com/blogApplication/BlogApplication/repository/RoleRepository.java
+++ src/main/java/com/blogApplication/BlogApplication/repository/RoleRepository.java
@@ -1,4 +1,12 @@
 package com.blogApplication.BlogApplication.repository;
 
-public interface RoleRepository {
+import com.blogApplication.BlogApplication.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository  extends JpaRepository<Role,Long> {
+
+
+    Optional<Role> findByname(String name);
 }
