f134153f957452735292947fd4a9124b15e7dc2e
Virendra Nagoriya <VirendraNagoriya11@gmail.com>
2026-01-05 13:30:50 +0530

    dto completed

--- src/main/java/com/blogApplication/BlogApplication/dto/PostRequestDto.java
+++ src/main/java/com/blogApplication/BlogApplication/dto/PostRequestDto.java
@@ -5,6 +5,14 @@ import jakarta.validation.constraints.NotBlank;
 public class PostRequestDto {
 
     @NotBlank
+   private String title;
 
+    @NotBlank
+    private String content;
+
+    @NotBlank
+    private String category ;
+
+    private List<String> tags;
 
 }
